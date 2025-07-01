ddd0ed7075662fa0a1f5afa58dc32c1611e5e943
funortehub <ian.santos2639@soufunorte.com.br>
2025-06-30 23:58:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,67 +1,62 @@
-const CACHE_NAME = 'funortehub-cache-v1.9.2';
+const CACHE_NAME = 'funorte-hub-cache-v1';
 const urlsToCache = [
-  '/',
-  'index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@300;400;500&display=swap',
-  'https://i.imgur.com/X7zXKVe.png',
-  'https://i.imgur.com/INUxxnc.png'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@300;400;500&display=swap',
+    'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js',
+    'https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js',
+    'https://i.imgur.com/X7zXKVe.png' // Funorte Logo
 ];
 
-// Instala o Service Worker e faz cache dos recursos estáticos
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Ativa o Service Worker e remove caches antigos
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Estratégia de cache-first para todas as requisições
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna o recurso do cache se encontrado
-        if (response) return response;
-        
-        // Clona a requisição para poder usá-la
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(response => {
-          // Verifica se recebemos uma resposta válida
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          // Clona a resposta para poder armazená-la no cache
-          const responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
+    event.waitUntil(
+        caches.open(CACHE_NAME)
             .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
 
-          return response;
-        });
-      })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
